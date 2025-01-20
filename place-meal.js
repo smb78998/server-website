cbc90dd7fd8ea59969cb76049fb72aa5ca6c6fc8
smb7898 <smb78998@gmail.com>
2025-01-19 21:41:13 -0500

    before adding cart js generate

--- place-meal.js
+++ place-meal.js
@@ -1,6 +1,3 @@
-
-
-
 let itemsHTML =''
 
 items.forEach((item)=>{
@@ -8,7 +5,7 @@ items.forEach((item)=>{
     <div class="grid-item">
           <p>${item.name}</p>
           <p>$${(item.price/100).toFixed(2)}</p>
-          <button class="js-add-item" data-item-name="${item.name}">Add Item</button>
+          <button class="js-add-item" data-item-name="${item.name}" data-item-price="${item.price}">Add Item</button>
         </div>
   `
 })
@@ -17,11 +14,13 @@ items.forEach((item)=>{
 document.querySelector('.js-selection-food').innerHTML= itemsHTML;
 
 
+//Create Cart 
 document.querySelectorAll('.js-add-item').forEach((button) => {
   button.addEventListener('click', () => {
 
     console.log("Added!");
     const itemName = button.dataset.itemName;
+   const itemPrice = button.dataset.itemPrice;
     console.log(button.dataset)
 
     let matchingItem;
@@ -38,6 +37,7 @@ document.querySelectorAll('.js-add-item').forEach((button) => {
       cart.push({
         itemName:itemName,
         quantity: 1,
+        itemPrice:itemPrice
       })
     }
    
@@ -45,3 +45,27 @@ document.querySelectorAll('.js-add-item').forEach((button) => {
 
   });
 });
+
+//attempt at the list on the side 
+
+let cartHTML =''
+
+cart.forEach((cart)=>{
+  cartHTML +=`
+   <div class="order-sum-info-row">
+
+          <p class="item-name">${cart.itemName}</p>
+
+          <div class="item-controls">
+            <button class="item-controls-button">-</button>
+            <p class="item-controls-quantity">${cart.quantity}</p>
+            <button class="item-controls-button">+</button>
+          </div>
+
+          <p class="item-price">${(cart.itemName)*cart.quantity}</p>
+
+        </div>
+  `
+})
+
+document.querySelector('.js-selection-food').innerHTML= itemsHTML;
